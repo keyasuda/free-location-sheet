05fc7934355f0122a8fc4ec65cd3da711649f703
keyasuda <keyasuda@users.noreply.github.com>
2021-05-02 19:28:05 +0900

    make printed editable

--- src/component/app/belongings/EditDialog.tsx
+++ src/component/app/belongings/EditDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import IconButton from '@material-ui/core/IconButton'
 import Icon from '@material-ui/core/Icon'
@@ -8,6 +8,9 @@ import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 
 const EditDialog = (props) => {
   const {
@@ -24,6 +27,7 @@ const EditDialog = (props) => {
   const nameRef = useRef()
   const descriptionRef = useRef()
   const quantitiesRef = useRef()
+  const [printed, setPrinted] = useState(item.printed)
 
   return (
     <Dialog open={open} onClose={handleClose}>
@@ -53,6 +57,19 @@ const EditDialog = (props) => {
             inputRef={quantitiesRef}
             defaultValue={1}
           />
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={printed}
+                  onChange={(e) => setPrinted(e.target.checked)}
+                  name="printed"
+                  color="primary"
+                />
+              }
+              label="印刷済み"
+            />
+          </FormGroup>
         </DialogContentText>
       </DialogContent>
 
@@ -72,6 +89,7 @@ const EditDialog = (props) => {
                   name: nameRef.current.value,
                   description: descriptionRef.current.value,
                   quantities: Number(quantitiesRef.current.value),
+                  printed: printed,
                 })
                 handleClose()
               }}
@@ -95,6 +113,7 @@ const EditDialog = (props) => {
                   name: nameRef.current.value,
                   description: descriptionRef.current.value,
                   quantities: Number(quantitiesRef.current.value),
+                  printed: printed,
                 })
                 handleClose()
               }}
--- src/component/app/storages/EditDialog.tsx
+++ src/component/app/storages/EditDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import IconButton from '@material-ui/core/IconButton'
 import Icon from '@material-ui/core/Icon'
@@ -8,11 +8,15 @@ import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 
 const EditDialog = (props) => {
   const { item, open, classes, handleClose, update } = props
   const nameRef = useRef()
   const descriptionRef = useRef()
+  const [printed, setPrinted] = useState(item.printed)
 
   return (
     <Dialog open={open} onClose={handleClose}>
@@ -33,6 +37,19 @@ const EditDialog = (props) => {
             defaultValue={item.description}
             className={classes.input}
           />
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={printed}
+                  onChange={(e) => setPrinted(e.target.checked)}
+                  name="printed"
+                  color="primary"
+                />
+              }
+              label="印刷済み"
+            />
+          </FormGroup>
         </DialogContentText>
       </DialogContent>
 
@@ -46,6 +63,7 @@ const EditDialog = (props) => {
               ...item,
               name: nameRef.current.value,
               description: descriptionRef.current.value,
+              printed: printed,
             })
             handleClose()
           }}
