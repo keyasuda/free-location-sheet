8cb4144af3d11a187e94a5e7e6b93fa8eb77f2b8
keyasuda <keyasuda@users.noreply.github.com>
2022-11-05 22:39:46 +0900

    set up ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles'
 
 import App from './component/App'
 import SignInButton from './component/SignInButton'
 import SheetList from './component/SheetList'
+
+const theme = createTheme()
 
 const IndexApp: React.FC = () => {
   return (
@@ -18,8 +21,18 @@ const IndexApp: React.FC = () => {
 
 const root = document.getElementById('index-app')
 if (root != null) {
-  ReactDOM.render(<IndexApp />, root)
+  ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <IndexApp />
+    </ThemeProvider>,
+    root
+  )
 } else {
   const appRoot = document.getElementById('app')
-  ReactDOM.render(<App />, appRoot)
+  ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+    appRoot
+  )
 }
